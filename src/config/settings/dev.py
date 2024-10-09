2d0796289fd3fff2bad2188140e0ec5b0c5dd1ec
Serhii Rozhnyi <rojnuys@gmail.com>
2024-10-09 22:21:36 +0300

    added: configuration of the project

---
+++ src/config/settings/dev.py
@@ -0,0 +1,43 @@
+from config.settings.base import *  # NOQA
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@t6sq6&fwebdtt!njz8-ix*qnq086b6a%#8i#2ct7nz84jw27g"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+INSTALLED_APPS += [  # NOQA
+    "django_extensions",
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [  # NOQA
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    PROJECT_DIR / "static",  # NOQA
+]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = PROJECT_DIR / "media"  # NOQA
---
+++ src/config/settings/stg.py
@@ -0,0 +1,32 @@
+from config.settings.base import *  # NOQA
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@t6sq6&fwebdtt!njz8-ix*qnq086b6a%#8i#2ct7nz84jw27g"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += []  # NOQA
+
+MIDDLEWARE += []  # NOQA
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = "static/"
+STATIC_ROOT = PROJECT_DIR / "static"  # NOQA
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = PROJECT_DIR / "media"  # NOQA
