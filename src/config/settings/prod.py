437181b720eaf5a1d630a49eb8a9b9a4af63e4e6
Serhii Rozhnyi <rojnuys@gmail.com>
2024-11-08 02:02:19 +0200

    added: nginx and two pages

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -1,12 +1,14 @@
+import os
+
 from config.settings.base import *  # NOQA
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@t6sq6&fwebdtt!njz8-ix*qnq086b6a%#8i#2ct7nz84jw27g"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost"]
 
 INSTALLED_APPS += []  # NOQA
 
